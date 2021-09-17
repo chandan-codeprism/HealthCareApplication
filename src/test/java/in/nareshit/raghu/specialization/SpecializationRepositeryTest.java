2cc6ed482769d591e85d4de946b038ff53f69dec
ChandanSince1996 <nckr2014@gmail.com>
2021-09-17 21:31:20 +0530

    test class

---
+++ src/test/java/in/nareshit/raghu/specialization/SpecializationRepositeryTest.java
@@ -0,0 +1,16 @@
+package in.nareshit.raghu.specialization;
+
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestMethodOrder(OrderAnnotation.class)
+public class SpecializationRepositeryTest {
+
+}
