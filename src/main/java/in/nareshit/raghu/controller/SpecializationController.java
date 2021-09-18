c7378f02fcd4d87002e8fb310c30c6362a080e28
ChandanSince1996 <nckr2014@gmail.com>
2021-09-19 01:25:26 +0530

    display spec

---
+++ src/main/java/in/nareshit/raghu/controller/SpecializationController.java
@@ -0,0 +1,32 @@
+package in.nareshit.raghu.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.service.ISpecializationService;
+
+@Controller
+@RequestMapping("/spec")
+public class SpecializationController {
+	
+	@Autowired
+	private ISpecializationService service;
+	
+	/**
+	 * 1. Display all Specializations
+	 */
+	
+	@GetMapping("/all")
+	public String viewAll(Model model) {
+		List<Specialization> list=service.getAllSpecialization();
+		model.addAttribute("list",list);
+		return "SpecializationData";
+	}
+
+}
