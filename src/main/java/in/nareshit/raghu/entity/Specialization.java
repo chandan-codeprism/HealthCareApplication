3b1d3c770d27a678165ba3d09bc9aeea1c186c5b
ChandanSince1996 <nckr2014@gmail.com>
2021-09-18 23:22:33 +0530

    junit test class wrote and repositery interface added

--- src/main/java/in/nareshit/raghu/entity/Specialization.java
+++ src/main/java/in/nareshit/raghu/entity/Specialization.java
@@ -7,10 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="specialization_tab")
 public class Specialization {
--- src/test/java/in/nareshit/raghu/specialization/SpecializationRepositeryTest.java
+++ src/test/java/in/nareshit/raghu/specialization/SpecializationRepositeryTest.java
@@ -1,17 +1,42 @@
 package in.nareshit.raghu.specialization;
 
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.TestMethodOrder;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import in.nareshit.raghu.entity.Specialization;
+import in.nareshit.raghu.repo.SpecializationRepositery;
+
 @DataJpaTest(showSql = true)
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @Rollback(false)
-@TestMethodOrder(OrderAnnotation.class)
 public class SpecializationRepositeryTest {
+	
+	@Autowired
+	private SpecializationRepositery repo;
+	
+	/**
+	 * 1. Test save operation
+	 */
+	@Test
+	@Order(1)
+	public void testSpecCreate() {
+		Specialization spec = new Specialization(null, "CRDLS", "Cardiologists", "Heart Doctor");
+		spec=repo.save(spec);
+		assertNotNull("This is not created", spec.getId());
+		
+		
+	}
+	
+	/**
+	 * 2. Test Display all operation
+	 */
 	
 	
 
