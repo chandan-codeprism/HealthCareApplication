f8295a9dc34b735956f48e5a870e1c9eaee812b8
ChandanSince1996 <nckr2014@gmail.com>
2021-09-19 18:50:22 +0530

    Show register page and save data in database

--- src/main/java/in/nareshit/raghu/controller/SpecializationController.java
+++ src/main/java/in/nareshit/raghu/controller/SpecializationController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.nareshit.raghu.entity.Specialization;
@@ -20,7 +22,30 @@ public class SpecializationController {
 	private ISpecializationService service;
 	
 	/**
-	 * 1. Display all Specializations
+	 * 1. Show Register page
+	 */
+	@GetMapping("/register")
+	public String displayRegister() {
+		return "SpecializationRegister";
+	}
+	
+	/**
+	 * 2. On Submit Form save data
+	 */
+	@PostMapping("/save")
+	public String saveForm(
+			@ModelAttribute Specialization specialization,
+			Model model
+			) 
+	{
+		Long id=service.savaSpecialization(specialization);
+		String message="Record ("+id+") is created";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
+	
+	/**
+	 * 3. Display all Specializations
 	 */
 	
 	@GetMapping("/all")
