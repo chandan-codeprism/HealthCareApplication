2b1a7cbd5204e995a9eb5d64c0a44626d4c982d8
ChandanSince1996 <nckr2014@gmail.com>
2021-09-20 19:41:43 +0530

    specialization repo class modified

--- src/main/java/in/nareshit/raghu/repo/SpecializationRepositery.java
+++ src/main/java/in/nareshit/raghu/repo/SpecializationRepositery.java
@@ -1,9 +1,13 @@
 package in.nareshit.raghu.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import in.nareshit.raghu.entity.Specialization;
 
 public interface SpecializationRepositery extends JpaRepository<Specialization, Long> {
+	
+	@Query("SELECT COUNT(specCode) FROM Specialization WHERE specCode=:specCode")
+	Integer getSpecCodeCount(String specCode);
 
 }
