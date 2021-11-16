f7011e8a4cb63ffb771841442cfa9fe2475c088f
Chandan Rai <nckr2014@gmail.com>
2021-11-16 19:52:25 +0530

    menubar setup

---
+++ src/main/java/in/nareshit/raghu/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * package in.nareshit.raghu.test;
+ * 
+ * import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ * 
+ * public class Test {
+ * 
+ * public static void main(String[] args) { String pwd = "CHANDAN";
+ * 
+ * BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); String encPwd =
+ * encoder.encode(pwd);
+ * 
+ * System.out.println(encPwd);
+ * 
+ * } }
+ */
